fc17ef3bcee98819b201cd948b5328de5e212d00
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-08-21 23:02:06 +0300

    style: changed background color

--- client/src/App.js
+++ client/src/App.js
@@ -41,10 +41,10 @@ function App() {
                 )}
                 <div className={
                     activeMenu
-                        ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
-                        : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+                        ? 'dark:bg-main-dark-bg  bg-slate-50 min-h-screen md:ml-72 w-full  '
+                        : ' bg-slate-50  dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
                 }>
-                    <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg-navbar w-full ">
+                    <div className="fixed md:static  bg-slate-50  dark:bg-main-dark-bg-navbar w-full ">
                         {showNavBar && <Navbar />}
                     </div>
 
