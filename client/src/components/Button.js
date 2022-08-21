4731a25019dc59d991f7764c21a1358b62b76ea9
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-08-21 22:49:22 +0300

    fix: debugged calendar page

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,10 +1,10 @@
 import React from 'react';
-export const Button = ({bgColor, color, size,text, borderRadius, onClick}) => {
+export const Button = ({bgColor, color, size,text, borderRadius, onClick, mb}) => {
     return(
     <>
     <button
     type="button"
-    style={{backgroundColor:bgColor, color, borderRadius }}
+    style={{backgroundColor:bgColor, color, borderRadius, marginBottom:mb }}
     className={`text-${size} p-3 hover:drop-shadow-x1`} 
     onClick={onClick}>
         {text}
--- client/src/pages/DnDCalendar.js
+++ client/src/pages/DnDCalendar.js
@@ -1,16 +1,18 @@
-import { Calendar , dateFnsLocalizer} from 'react-big-calendar'
+import { Calendar, globalizeLocalizer } from 'react-big-calendar'
+import { DateTime, Settings } from 'luxon'
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.css'
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import format from "date-fns/format";
-import getDay from "date-fns/getDay";
-import parse from "date-fns/parse";
-import startOfWeek from "date-fns/startOfWeek";
-import React, { useEffect } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTasksAction } from '../redux/taskSlices';
 import { useStateContext } from '../context/ContextProvider';
-import TaskCard from '../components/kanban/TaskCard';
+import CreateTask from '../components/kanban/createTask';
+import DeleteDialogBox from '../components/DeleteDialogBox';
+import { TasksInformation } from '../components/kanban/TaskInformation';
+import globalize from 'globalize'
+import { Button } from '../components/Button';
+import { MdCancel } from 'react-icons/md';
 
 
 
@@ -18,51 +20,107 @@ import TaskCard from '../components/kanban/TaskCard';
 
 
 export default function DnDCalendar() {
+  const { showModal, showDeleteModal, currentEntry, tasks, setTasks, showInfoModal, currentColor } = useStateContext();
+  const [showNoDateModal, setShowNoDateModal] = useState(false)
   const DnDCalendarComponent = withDragAndDrop(Calendar)
-  const locales = {
-    "en-US": require("date-fns/locale/en-US"),
-};
-const localizer = dateFnsLocalizer({
-    format,
-    parse,
-    startOfWeek,
-    getDay,
-    locales,
-});
-const {tasks, setTasks} = useStateContext();
-const dispatch= useDispatch()
-useEffect(()=> {
-dispatch(fetchTasksAction())
-}, [])
-const tasksState = useSelector((state) => state?.tasks)
-const { tasksFetched, taskLoading, taskAppErr, taskServerErr } = tasksState
-useEffect(() => {
-  if(tasksFetched) {
-     setTasks(tasksFetched?.tasks)
+
+  const events = tasks?.map(task => {
+
+    return {
+      start: new Date(Date.parse(task?.start)),
+      end: new Date(Date.parse(task?.end)),
+      title: task?.title,
+      allDay: function () {
+        if (this.start === this.end) {
+          return true
+        } else {
+          return false
+        }
+      }
+
+
+    }
+
   }
- 
- }, [tasksFetched, setTasks])
+  );
+  console.log(events)
+  const defaultTZ = DateTime.local().zoneName
+  const defaultDateStr = '2015-04-13'
 
- const events=tasks.map(task=> {return task})
-return (
-  <div className=" w-10/12 mt-5 ml-10">
-    <h1>Tasks with no timelines(click to edit)</h1>
-    <div className='width-5/6 h-32 bg-gradient-to-r from-indigo-100 via-purple-50 to-pink-50 mb-3 rounded-lg flex flex-row flex-wrap justify-between'>
-{tasks?.filter(task => !task?.start && task?.status !=="Done")?.map(task => 
-  <div className="bg-white rounded m-5 p-1 shadow-2xl text-xs h-8"   key={task?.taskId} >
-  <span>{task?.title}</span>
-  </div>
+  function getDate(str, DateTimeObj) {
+    return DateTimeObj.fromISO(str).toJSDate()
+  }
 
-)}
+
+  const [timezone, setTimezone] = useState(defaultTZ)
+
+  const { getNow, localizer, scrollToTime } =
+    useMemo(() => {
+      Settings.defaultZone = timezone
+      return {
+        defaultDate: getDate(defaultDateStr, DateTime),
+        getNow: () => DateTime.local().toJSDate(),
+        localizer: globalizeLocalizer(globalize),
+        scrollToTime: DateTime.local().toJSDate(),
+      }
+    }, [timezone])
+
+  useEffect(() => {
+    return () => {
+      Settings.defaultZone = defaultTZ // reset to browser TZ on unmount
+    }
+  }, [])
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchTasksAction())
+  }, [])
+  const tasksState = useSelector((state) => state?.tasks)
+  const { tasksFetched } = tasksState
+  useEffect(() => {
+    if (tasksFetched) {
+      setTasks(tasksFetched?.tasks)
+    }
+
+  }, [tasksFetched, setTasks])
+
+  return (
+    <div className=" w-10/12 mt-5 ml-10">
+
+      {!showNoDateModal && <Button
+        bgColor={currentColor}
+        text="Show tasks with no Timelines"
+        borderRadius="10px"
+        mb="20px" 
+        onClick={()=> setShowNoDateModal(true)} />}
+
+      {showNoDateModal && <div className='width-5/6 h-32 bg-gradient-to-r from-indigo-100 via-purple-50 to-pink-50 mb-3 rounded-lg flex flex-row flex-wrap justify-between'>
+        <div>
+        <MdCancel className='close-icon' color='red' onClick={() => {            
+                setShowNoDateModal(false);          
+            }} 
+            style={{cursor: "pointer"}}/>
+        {tasks?.filter(task => !task?.start && task?.status !== "Done")?.map(task =>
+          <div className="bg-white rounded m-5 p-1 shadow-2xl text-xs h-8" key={task?.taskId} >
+            <span>{task?.title}</span>
+          </div>
+
+        )}
+        </div>
+      </div>}
+      <DnDCalendarComponent
+        localizer={localizer}
+        events={events}
+        getNow={getNow}
+        draggableAccessor={(event) => true}
+        startAccessor="start" endAccessor="end"
+        allDayAccessor="allDay"
+        style={{ height: "60vh" }}
+        scrollToTime={scrollToTime}
+        popup
+      />
+      {showModal && <CreateTask />}
+      {showDeleteModal && <DeleteDialogBox task={currentEntry} item="Task" />}
+      {showInfoModal && <TasksInformation />}
     </div>
-  <DnDCalendarComponent 
-    localizer={localizer}
-    events={events}
-    draggableAccessor={(event) => true}
-    startAccessor="start" endAccessor="end"
-    style={{ height: "60vh"}} 
-    popup
-  />
-  </div>
-)
+  )
 }
