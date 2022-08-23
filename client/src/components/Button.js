642122d79f45c73b0713603d08ece4b686844324
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-08-23 23:22:06 +0300

    style: added more properties

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,11 +1,11 @@
 import React from 'react';
-export const Button = ({bgColor, color, size,text, borderRadius, onClick, mb}) => {
+export const Button = ({bgColor, color, size,text, borderRadius, onClick, mb, type, padding}) => {
     return(
     <>
     <button
     type="button"
     style={{backgroundColor:bgColor, color, borderRadius, marginBottom:mb }}
-    className={`text-${size} p-3 hover:drop-shadow-x1`} 
+    className={`text-${size} py-2 px-4 hover:drop-shadow-x1`} 
     onClick={onClick}>
         {text}
     </button>
