d2a9dbe0901e46dd143482731b603a7d550a0e33
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-08-23 08:25:52 +0300

    fix: added a route to edit user Data

--- Server/routes/Users.js
+++ Server/routes/Users.js
@@ -1,7 +1,7 @@
 const express= require('express')
 
 const authentication = require('../middlewear/authentication');
-const { registerUserCtrl, createProfileCtrl, loginUserCtrl, fetchUserCtrl } = require('../controllers/Users');
+const { registerUserCtrl, createProfileCtrl, loginUserCtrl, fetchUserCtrl , upDateProfileCtrl} = require('../controllers/Users');
 const Multer = require('../utils/multer');
 const upload = Multer.single('image');
 userRoutes=express.Router()
@@ -11,6 +11,7 @@ userRoutes=express.Router()
 
 userRoutes.post("/register", registerUserCtrl)
 userRoutes.put("/", upload,  authentication, createProfileCtrl )
+userRoutes.put("/update/", upload,  authentication, upDateProfileCtrl)
 userRoutes.post ("/login", loginUserCtrl)
 userRoutes.get("/profile", authentication, fetchUserCtrl)
 
