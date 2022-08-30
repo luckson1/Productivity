94d970819d93d982c2ec9b4474ac75d4a60ea307
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-08-30 13:47:04 +0300

    fix: removed unused components

--- client/src/components/DeleteDialogBox.js
+++ client/src/components/DeleteDialogBox.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { useDispatch } from "react-redux"
 import { useStateContext } from '../context/ContextProvider';
-import { deleteExpenseAction } from '../redux/expenseSlices';
-import { deleteIncomeAction } from '../redux/IncomeSlices';
+
 import { deleteTaskAction } from '../redux/taskSlices';
 function DeleteDialogBox({ item, shoppingItem, task, entry, }) {
 
     // dispatch action to delete task
     const dispatch = useDispatch()
-    const { setShowDeleteModal, isExpense, tasks,setTasks, incomes, setIncomes,  expenses, setExpenses } = useStateContext();
+    const { setShowDeleteModal,  tasks,setTasks } = useStateContext();
 
     const deleteTaskHandler= ()=>{
         dispatch(deleteTaskAction(task))
@@ -19,18 +18,7 @@ function DeleteDialogBox({ item, shoppingItem, task, entry, }) {
 
    
 
-    const deleteIncomeHandler = () => {
-        dispatch(deleteIncomeAction(entry));
-        const newIncomeList=incomes.filter(income=> income._id !==entry?._id);
-        setIncomes( newIncomeList);
-        setShowDeleteModal(false)
-    }
-    const deleteExpenseHandler = () => {
-        dispatch(deleteExpenseAction(entry));
-        const newExpenseList=expenses.filter(expense=> expense._id !==entry?._id);
-        setExpenses( newExpenseList);
-        setShowDeleteModal(false)
-    }
+
     
     return (
         <div>
@@ -41,8 +29,6 @@ function DeleteDialogBox({ item, shoppingItem, task, entry, }) {
                     <p>Are you sure you want to delete this {item}</p>
                     <button className="del" onClick={
                         task !== undefined ? () =>deleteTaskHandler()
-                                : entry !== undefined && isExpense ? () => deleteExpenseHandler()
-                                    : entry !== undefined && !isExpense ? () => deleteIncomeHandler()
                                         : console.log("error")
                     }>Delete {item}</button>
                     <button className="cancel" onClick={() => setShowDeleteModal(false)}>Cancel</button>
