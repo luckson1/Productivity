d56315c33b1d7f4070c71ffbf531bd393d7e19b6
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-09-01 23:54:53 +0300

    fix: add assign team mate feature

--- client/src/components/bagtracker/BugEntryComponent.js
+++ client/src/components/bagtracker/BugEntryComponent.js
@@ -1,109 +1,149 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from "react-redux"
-import { fetchbugsAction } from '../../redux/bugsSlices';
-import BugCard from './BugCard'
-import { ItemTypes } from '../../utils/items'
-import { useStateContext } from '../../context/ContextProvider';
-import CreateBugEntry from './CreateBugEntry';
-import { BugsInformation } from './BugsInformation';
-import ReviewBugs from './ReviewBugs';
-import ClosedBugs from './ClosedBug';
-import { Button } from '../Button';
-import moment from "moment"
-import { fetchTeamMembersAction } from '../../redux/usersSlices';
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchbugsAction } from "../../redux/bugsSlices";
+import BugCard from "./BugCard";
+import { ItemTypes } from "../../utils/items";
+import { useStateContext } from "../../context/ContextProvider";
+import CreateBugEntry from "./CreateBugEntry";
+import { BugsInformation } from "./BugsInformation";
+import ReviewBugs from "./ReviewBugs";
+import ClosedBugs from "./ClosedBug";
+import { Button } from "../Button";
+import moment from "moment";
+import { fetchTeamMembersAction } from "../../redux/usersSlices";
+import InProgressBugs from "./InProgressBugs";
 
 export default function BugEntryComponent() {
-    // display or remove action creation/edit form 
+  // display or remove action creation/edit form
 
+  const {
+    currentColor,
+    showModal,
+    setShowModal,
+    isEdit,
+    setIsEdit,
+    currentEntry,
+    bugs,
+    setBugs,
+    showInfoModal,
+    setTeam,
+  } = useStateContext();
 
-    const { currentColor, showModal, setShowModal, isEdit, setIsEdit, currentEntry, bugs, setBugs, showInfoModal, setTeam} = useStateContext();
+  // dispatch action to fetch all Bugs
+  const dispatch = useDispatch();
 
-    // dispatch action to fetch all Bugs
-    const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchbugsAction());
+  }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchTeamMembersAction());
+  }, []);
 
+  const bugsState = useSelector((state) => state?.bugs);
+  const { bugsFetched, bugLoading, bugAppErr, bugServerErr } = bugsState;
 
-    useEffect(() => {
-        dispatch(fetchbugsAction())
+  useEffect(() => {
+    if (bugsFetched) {
+      setBugs(bugsFetched?.bugs);
+    }
+  }, [bugsFetched, setBugs]);
 
-    }, [dispatch])
-    useEffect(()=> {
-        dispatch(fetchTeamMembersAction())
-        }, [])
+  const openBugs = bugs?.filter((bug) => bug?.status === "Open");
+  const inProgressBugs = bugs?.filter((bug) => bug?.status === "In Progress");
+  const inReviewBugs = bugs?.filter((bug) => bug?.status === "In Review");
+  const closedBugs = bugs?.filter(
+    (bug) =>
+      bug?.status === "Closed" &&
+      new Date(bug?.updatedAt) >
+        new Date(moment().subtract(7, "days").calendar())
+  );
 
-    const bugsState = useSelector((state) => state?.bugs)
-    const { bugsFetched, bugLoading, bugAppErr, bugServerErr } = bugsState
+  const teamMembers = useSelector(
+    (state) => state?.users?.teamProfile?.teamMembers
+  );
+  useEffect(() => {
+    if (typeof teamMembers !== "undefined")
+      setTeam(teamMembers.filter((member) => member?.status !== "Pending"));
+  }, [teamMembers]);
 
-    useEffect(() => {
-        if (bugsFetched) {
-            setBugs(bugsFetched?.bugs)
-        }
+  return (
+    <div className=" w-11/12  mx-3 text-sm md:text-base md:flex-nowrap mt-24">
+      <div className="kanban-heading">
+        <Button
+          bgColor={currentColor}
+          borderRadius="10px"
+          text="Add New Bug"
+          onClick={() => {
+            setShowModal(true);
+            window.scrollTo(0, 0);
+          }}
+        />
+      </div>
 
-    }, [bugsFetched, setBugs])
+      <div className="kanban-board ">
+        <div className="kanban-block bg-indigo-100 shadow-md">
+          <strong>Open</strong>
 
-
-    const openBugs = bugs?.filter(bug => bug?.status === "Open")
-    const inReviewBugs = bugs?.filter(bug => bug?.status === "In Review" )
-    const closedBugs = bugs?.filter(bug => bug?.status === "Closed" && new Date(bug?.updatedAt) > new Date(moment().subtract(7, 'days').calendar()))
-
-
-    const teamMembers= useSelector(state=> state?.users?.teamProfile?.teamMembers)
-    useEffect(()=> {
-      if (typeof teamMembers !== "undefined") setTeam(teamMembers.filter(member=> member?.status !=="Pending"))
-    }, [teamMembers])
-  
-   
-
-
-    return (
-
-        <div className=" w-11/12  mx-3 text-sm md:text-base md:flex-nowrap mt-24">
-            <div className="kanban-heading">
-                <Button bgColor={currentColor} borderRadius="10px" text="Add New Bug" onClick={() => { setShowModal(true); window.scrollTo(0, 0) }} />
+          {bugAppErr || bugServerErr ? (
+            <div className="form-validation">An Error Has Occured</div>
+          ) : bugLoading ? (
+            <h4>Loading Please Wait......</h4>
+          ) : openBugs?.length === 0 ? (
+            <div>
+              <h3>No Bugs to Display, Please create some </h3>
             </div>
-
-            <div className="kanban-board ">
-                <div className="kanban-block bg-indigo-100 shadow-md">
-                    <strong>Open</strong>
-
-                    {bugAppErr || bugServerErr ? (<div className="form-validation">An Error Has Occured</div>)
-                        : bugLoading ? <h4>Loading Please Wait......</h4>
-                            : openBugs?.length === 0 ? (<div><h3>No Bugs to Display, Please create some </h3></div>)
-                                : openBugs?.map(bug => (<BugCard
-                                    bug={bug}
-                                    key={bug?.bugId}
-                                    type={ItemTypes.OPEN}
-
-                                />))}
-
-                </div>
-                <ReviewBugs  >
-                    <strong>In Review</strong>
-                    {bugAppErr || bugServerErr ? (<div className="form-validation">An Error Has Occured</div>)
-                        : bugLoading ? <h4>Loading Please Wait......</h4>
-                            : inReviewBugs?.map(bug => (<BugCard
-                                bug={bug}
-                                key={bug?.bugId}
-                                type={ItemTypes.REVIEW}
-
-
-                            />))}
-                </ReviewBugs>
-                <ClosedBugs>
-                    <strong>Closed</strong>
-                    {bugAppErr || bugServerErr ? (<div className="form-validation">An Error Has Occured</div>)
-                        : bugLoading ? <h4>Loading Please Wait......</h4>
-                            : closedBugs?.map(bug => (<BugCard
-                                bug={bug}
-                                key={bug?.bugId}
-                                type={ItemTypes.CLOSED}
-                            />))}
-
-                </ClosedBugs>
-            </div>
-            {showModal && <CreateBugEntry setShowModal={setShowModal} isEdit={isEdit} entry={currentEntry} setIsEdit={setIsEdit} />}
-            {showInfoModal && <BugsInformation bugEntry={currentEntry} />}
+          ) : (
+            openBugs?.map((bug) => (
+              <BugCard bug={bug} key={bug?.bugId} type={ItemTypes.OPEN} />
+            ))
+          )}
         </div>
-
-    )
+        <InProgressBugs>
+          <strong>In Progress</strong>
+          {bugAppErr || bugServerErr ? (
+            <div className="form-validation">An Error Has Occured</div>
+          ) : bugLoading ? (
+            <h4>Loading Please Wait......</h4>
+          ) : (
+            inProgressBugs?.map((bug) => (
+              <BugCard bug={bug} key={bug?.bugId} type={ItemTypes.PROGRESS} />
+            ))
+          )}
+        </InProgressBugs>
+        <ReviewBugs>
+          <strong>Testing/Review</strong>
+          {bugAppErr || bugServerErr ? (
+            <div className="form-validation">An Error Has Occured</div>
+          ) : bugLoading ? (
+            <h4>Loading Please Wait......</h4>
+          ) : (
+            inReviewBugs?.map((bug) => (
+              <BugCard bug={bug} key={bug?.bugId} type={ItemTypes.REVIEW} />
+            ))
+          )}
+        </ReviewBugs>
+        <ClosedBugs>
+          <strong>Closed</strong>
+          {bugAppErr || bugServerErr ? (
+            <div className="form-validation">An Error Has Occured</div>
+          ) : bugLoading ? (
+            <h4>Loading Please Wait......</h4>
+          ) : (
+            closedBugs?.map((bug) => (
+              <BugCard bug={bug} key={bug?.bugId} type={ItemTypes.CLOSED} />
+            ))
+          )}
+        </ClosedBugs>
+      </div>
+      {showModal && (
+        <CreateBugEntry
+          setShowModal={setShowModal}
+          isEdit={isEdit}
+          entry={currentEntry}
+          setIsEdit={setIsEdit}
+        />
+      )}
+      {showInfoModal && <BugsInformation bugEntry={currentEntry} />}
+    </div>
+  );
 }
