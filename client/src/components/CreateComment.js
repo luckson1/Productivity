f1b9e49ae8ac7960be327c450a084f98f718b3a9
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-09-01 13:17:31 +0300

    fix: create comment component

---
+++ client/src/components/CreateComment.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import * as Yup from 'yup'
+import { useFormik } from 'formik';
+import { useDispatch } from 'react-redux';
+import { useStateContext } from '../context/ContextProvider';
+import { v4 as uuidv4 } from "uuid";
+import "react-datepicker/dist/react-datepicker.css";
+import { createCommentAction } from '../redux/CommentSlices';
+const errorSchema = Yup.object().shape({
+
+    details: Yup
+        .string()
+        .required('Comment Required'),
+
+});
+function CreateComment({bugId}) {
+    const { currentColor } = useStateContext()
+
+    const dispatch = useDispatch()
+
+
+    const addTaskHandler = (values) => {
+        dispatch(createCommentAction(values))
+    }
+    // use formik hook to handle form state 
+    const formik = useFormik({
+        initialValues: {
+
+            details: '',
+            commentId: uuidv4(),
+            bugId: bugId
+        },
+        validationSchema: errorSchema,
+        onSubmit: (values, { resetForm }) => { addTaskHandler(values); resetForm({ values: "" }) }
+    });
+
+
+    return (<>
+
+        <form onSubmit={formik.handleSubmit} className=" dark:bg-[#484B52] flex flex-row px-0 ">
+
+
+            <div className="flex flex-row justify-between gap-2 border-1 border-slate-50 items-end" style={{width: "100%"}}>
+                <textarea className=" shadow-2xl rounded-md px-1 text-xs bg-white h-24"
+                    type="text"
+                    id="name"
+                    value={formik.values.details}
+                    onChange={formik.handleChange("details")}
+                    onBlur={formik.handleBlur("details")}
+                    placeholder='Add a comment'
+                    style={{width: "90%"}} ></textarea>
+
+                <button type="submit" className='  rounded-md shadow-2xl h-10 animate-bounce text-white'  style={{backgroundColor: currentColor}} >
+                     Add Comment
+                     </button>
+
+            </div>
+
+
+        </form>
+        {/* errors */}
+        <div className="form-validation">
+            {formik.touched.details && formik.errors.details}
+        </div>
+    </>
+
+    )
+}
+
+export default CreateComment
