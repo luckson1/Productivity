43b28d9816a7a71c8618cf44fe378b3c85d3d1bf
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-09-01 23:54:04 +0300

    fix: add comment feature and in progress status

--- client/src/components/bagtracker/BugsInformation.js
+++ client/src/components/bagtracker/BugsInformation.js
@@ -1,106 +1,151 @@
-import React, { useEffect } from 'react'
-import { MdCancel } from 'react-icons/md';
-import { useDispatch, useSelector } from 'react-redux';
-import { useStateContext } from '../../context/ContextProvider';
-import { editBugsAction } from '../../redux/bugsSlices';
-import { fetchCommentAction } from '../../redux/CommentSlices';
-import { Button } from '../Button'
-import CreateComment from '../CreateComment';
-import InfoCard from '../InfoCard';
+import React, { useEffect } from "react";
+import { MdCancel } from "react-icons/md";
+import { useDispatch, useSelector } from "react-redux";
+import { useStateContext } from "../../context/ContextProvider";
+import { editBugsAction } from "../../redux/bugsSlices";
+import { fetchCommentAction } from "../../redux/CommentSlices";
+import { Button } from "../Button";
+import CreateComment from "../CreateComment";
+import InfoCard from "../InfoCard";
 
+export const BugsInformation = ({ bugEntry }) => {
+  const {
+    currentColor,
+    setShowInfoModal,
+    setIsEdit,
+    setShowModal,
+    setCurrentEntry,
+    bugs,
+    currentEntry,
+    setBugs,
+    team,
+  } = useStateContext();
+  const entry = currentEntry;
+  const dispatch = useDispatch();
+  const newStatus =
+    bugEntry?.status === "Open"
+      ? "In Progress"
+      : bugEntry?.status === "In Progress"
+      ? "In Review"
+      : bugEntry?.status === "In Review"
+      ? "Closed"
+      : "Open";
+  const editbugHandler = (bugEntry) => {
+    const values = {
+      title: bugEntry.title,
+      description: bugEntry.description,
+      steps: bugEntry.steps,
+      status: newStatus,
+      priority: bugEntry.priority,
+      assigned: bugEntry.assigned,
+      createdAt: bugEntry.createdAt,
+      updatedAt: new Date(),
+      _id: bugEntry._id,
+      bugId: bugEntry.bugId,
+    };
+    dispatch(editBugsAction(values));
+    const newBugs = bugs?.filter((bug) => {
+      return entry._id !== bug?._id;
+    });
 
-export const BugsInformation = ({ bugEntry, }) => {
-    const { currentColor, setShowInfoModal, setIsEdit, setShowModal, setCurrentEntry, bugs, currentEntry, setBugs, team } = useStateContext();
-    const entry = currentEntry
-    const dispatch = useDispatch()
-    const newStatus = bugEntry?.status === "Open" ? "In Review" : bugEntry?.status === "In Review" ? "Closed" : "Open"
-    const editbugHandler = (bugEntry) => {
-        const values = {
-            title: bugEntry.title,
-            description: bugEntry.description,
-            steps: bugEntry.steps,
-            status: newStatus,
-            priority: bugEntry.priority,
-            assigned: bugEntry.assigned,
-            createdAt: bugEntry.createdAt,
-            updatedAt: new Date(),
-            _id: bugEntry._id,
-            bugId: bugEntry.bugId
-        }
-        dispatch(editBugsAction(values));
-        const newBugs = bugs?.filter(bug => {
-            return entry._id !== bug?._id
-        })
+    setBugs([...newBugs, values]);
+    setShowInfoModal(false);
+  };
 
-        setBugs([...newBugs, values]);
-        setShowInfoModal(false);
+  useEffect(() => {
+    dispatch(fetchCommentAction({ id: bugEntry?.bugId }));
+  }, []);
 
-    }
-console.log(bugEntry?.bugId)
- useEffect(()=> {
-dispatch(fetchCommentAction({id:bugEntry?.bugId}))
- }, [])
+  const comments = useSelector(
+    (state) => state?.comment?.commentsFetched?.comment
+  );
+  const assigneeData = team?.filter(
+    (member) => member?._id === bugEntry?.assigned
+  );
 
+  return (
+    <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
+      <div className="float-right h-screen  bg-gradient-to-r from-blue-100 via-pink-100 to-indigo-50  dark:bg-[#484B52] w-full sm:w-6/12 overflow-scroll">
+        <div className="flex justify-between items-center gap-2 mt-7 mx-7">
+          <p className="text-xl font-semibold text-gray-900 text-center">
+            Bug Information
+          </p>
 
-const comments= useSelector(state=> state?.comment?.commentsFetched?.comment)
-    const assigneeData = team?.filter(member => member?._id === bugEntry?.assigned)
-
-    return (
-        <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
-            <div className="float-right h-screen  bg-gradient-to-r from-blue-100 via-pink-100 to-indigo-50  dark:bg-[#484B52] w-full sm:w-6/12 overflow-scroll">
-                <div className="flex justify-between items-center gap-2 mt-7 mx-7">
-                    <p className="text-xl font-semibold text-gray-900 text-center">Bug Information</p>
-
-                    <MdCancel className='' color='red' size="30px" style={{ cursor: "pointer" }} onClick={() => {
-                        setIsEdit(false);
-                        setShowInfoModal(false);
-
-
-                    }} />
-                </div>
-
-                <div className="mt-3 text-sm mx-7 dark:text-gray-200">
-
-                    <div className="flex-col">
-                        <div className="flex flex-row gap-2 flex-wrap">
-                            <InfoCard title="Title" details={bugEntry?.title} />
-                            <InfoCard title="Priority" details={bugEntry?.priority} />
-                            <InfoCard title="Status" details={bugEntry?.status} />
-                            <InfoCard title="Assignee" details={assigneeData[0]?.firstName ?? "Not Assigned"} />
-                        </div>
-
-                        <InfoCard title="Description" details={bugEntry?.description} />
-                        <InfoCard title="Steps" details={bugEntry?.steps} />           
-                        <h2>Comments</h2>
-                       { comments?.map(comment => <InfoCard title= {team?.filter(member => member?._id === comment?.creator)[0]?.firstName} details={comment?.details} /> )}
-                        <CreateComment bugId={bugEntry?.bugId}/>
-
-
-                    </div>
-
-                </div>
-                <div className="flex justify-between items-center mt-3 border-t-1 border-color mx-7">
-
-                    <Button
-                        color="white"
-                        bgColor={currentColor}
-                        text="Edit Details"
-                        borderRadius="10px"
-                        onClick={() => { setShowInfoModal(false); setShowModal(true); setIsEdit(true); setCurrentEntry(bugEntry); window.scrollTo(0, 0) }}
-                    />
-                    <Button
-                        color="white"
-                        bgColor={currentColor}
-                        text={bugEntry?.status === "Open" ? "Send to Review" : bugEntry?.status === "In Review" ? "Mark as Complete" : bugEntry?.status === "Closed" ? "Re-Open" : null}
-                        borderRadius="10px"
-                        onClick={() => { editbugHandler(bugEntry) }}
-                    />
-
-
-
-                </div>
-            </div>
+          <MdCancel
+            className=""
+            color="red"
+            size="30px"
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              setIsEdit(false);
+              setShowInfoModal(false);
+            }}
+          />
         </div>
 
-    )
-}
+        <div className="mt-3 text-sm mx-7 dark:text-gray-200">
+          <div className="flex-col">
+            <div className="flex flex-row gap-2 justify-between flex-wrap">
+              <InfoCard title="Title" details={bugEntry?.title} />
+              <InfoCard title="Priority" details={bugEntry?.priority} />
+              <InfoCard title="Status" details={bugEntry?.status} />
+              <InfoCard
+                title="Assignee"
+                details={assigneeData[0]?.firstName ?? "Not Assigned"}
+              />
+            </div>
+
+            <InfoCard title="Description" details={bugEntry?.description} />
+            <InfoCard title="Steps" details={bugEntry?.steps} />
+            <h2>Comments</h2>
+            {comments?.map((comment) => (
+              <InfoCard
+                key={comment?.commentId}
+                title={
+                  team?.filter((member) => member?._id === comment?.creator)[0]
+                    ?.firstName
+                }
+                details={comment?.details}
+              />
+            ))}
+            <CreateComment bugId={bugEntry?.bugId} />
+          </div>
+        </div>
+        <div className="flex justify-between items-center mt-3 border-t-1 border-color mx-7">
+          <Button
+            color="white"
+            bgColor={currentColor}
+            text="Edit Details"
+            borderRadius="10px"
+            onClick={() => {
+              setShowInfoModal(false);
+              setShowModal(true);
+              setIsEdit(true);
+              setCurrentEntry(bugEntry);
+              window.scrollTo(0, 0);
+            }}
+          />
+          <Button
+            color="white"
+            bgColor={currentColor}
+            text={
+              bugEntry?.status === "Open"
+                ? "Add to Progress"
+                : bugEntry?.status === "In Review"
+                ? "Mark as Complete"
+                : bugEntry?.status === "In Progress"
+                ? "Send to Review"
+                : bugEntry?.status === "Closed"
+                ? "Re-Open"
+                : null
+            }
+            borderRadius="10px"
+            onClick={() => {
+              editbugHandler(bugEntry);
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
