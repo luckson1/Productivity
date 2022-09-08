8aca9debf1c87de96224b7c3f073f8a1dc70a981
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-09-08 22:12:50 +0300

    fix: add member profile image and style

--- client/src/components/users/TeamList.js
+++ client/src/components/users/TeamList.js
@@ -1,41 +1,46 @@
-import React from 'react'
-import { useStateContext } from '../../context/ContextProvider';
-
-
-
-
-
+import React from "react";
+import { useStateContext } from "../../context/ContextProvider";
 
 function TeamList() {
-  const {  team} = useStateContext();
+  const { team } = useStateContext();
 
   return (
     <div className="table">
-
       <ul className="responsive-table">
         <li className="table-header">
+          <div className="col col-1">Avatar</div>
           <div className="col col-1">Name</div>
           <div className="col col-2">Email</div>
           <div className="col col-3">Role</div>
           <div className="col col-4">Status</div>
-
         </li>
 
-        {team?.map(member=> (<li className="table-row" key={member?.userId} >
-                <div className="col col-1" data-label="Name">{member?.firstName??member?.name}</div>
-                <div className="col col-2" data-label="Email">{member?.email}</div>
-                <div className="col col-3" data-label="Role" >{member?.role??"Not Defined"} </div>
-                <div className="col col-4" data-label="Status">{member?.status ?? "Approved"}</div>
-          
-            
-
-              </li>))}
-
-
+        {team?.map((member) => (
+          <li className="table-row" key={member?.userId}>
+            <div className="col col-1" data-label="Avatar">
+              <img
+                alt="profile iimage"
+                src={member?.image}
+                className="rounded-full h-6 w-6 ml-5"
+              />
+            </div>
+            <div className="col col-1" data-label="Name">
+              {member?.firstName ?? member?.name}
+            </div>
+            <div className="col col-2" data-label="Email">
+              {member?.email}
+            </div>
+            <div className="col col-3" data-label="Role">
+              {member?.role ?? "Not Defined"}
+            </div>
+            <div className="col col-4" data-label="Status">
+              {member?.status ?? "Approved"}
+            </div>
+          </li>
+        ))}
       </ul>
-
     </div>
-  )
+  );
 }
 
-export default TeamList
+export default TeamList;
