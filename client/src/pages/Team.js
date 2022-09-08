284e261d3afd2cf38a3bd50284184f7ed1bb2617
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-09-08 22:12:23 +0300

    style: format

--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -1,41 +1,44 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Button } from '../components/Button'
-import InviteUser from '../components/users/InviteUser'
-import TeamList from '../components/users/TeamList'
-import { useStateContext } from '../context/ContextProvider'
-import { fetchTeamMembersAction, isShowModal } from '../redux/usersSlices'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Button } from "../components/Button";
+import InviteUser from "../components/users/InviteUser";
+import TeamList from "../components/users/TeamList";
+import { useStateContext } from "../context/ContextProvider";
+import { fetchTeamMembersAction, isShowModal } from "../redux/usersSlices";
 
 function Team() {
-  const { currentColor, setTeam} = useStateContext();
+  const { currentColor, setTeam } = useStateContext();
 
-  const dispatch = useDispatch()
-  useEffect(()=> {
-  dispatch(fetchTeamMembersAction())
-  }, [])
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchTeamMembersAction());
+  }, []);
 
-const teamData= useSelector(state=> state?.users)
-const {teamProfile, showModal}=teamData
-const teamMembers=teamProfile?.teamMembers
-useEffect(()=> {
-  if (typeof teamMembers !== "undefined") setTeam(teamMembers)
-}, [teamMembers])
-
+  const teamData = useSelector((state) => state?.users);
+  const { teamProfile, showModal } = teamData;
+  const teamMembers = teamProfile?.teamMembers;
+  useEffect(() => {
+    if (typeof teamMembers !== "undefined") setTeam(teamMembers);
+  }, [teamMembers]);
 
   return (
     <div>
-  
-      <Button onClick={() => {dispatch(isShowModal())}} text="Add Member" bgColor={currentColor} borderRadius="10px" animationType="bounce"/>
-      <div className='mt-7'>
-     
+      <Button
+        onClick={() => {
+          dispatch(isShowModal());
+        }}
+        text="Add Member"
+        bgColor={currentColor}
+        borderRadius="10px"
+        animationType="bounce"
+      />
+      <div className="mt-7">
         <TeamList />
       </div>
 
-
       {showModal && <InviteUser />}
-
     </div>
-  )
+  );
 }
 
-export default Team
+export default Team;
