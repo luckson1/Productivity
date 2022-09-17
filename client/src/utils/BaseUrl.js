2fd4efe8ba1449bed8862c01eb04aee191049648
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-09-17 03:37:18 +0300

    fix: try new cloud provider

--- client/src/utils/BaseUrl.js
+++ client/src/utils/BaseUrl.js
@@ -1,2 +1,3 @@
-export const BaseURL= "https://techivity.herokuapp.com/api"
-export const ExpensesURL= "https://productivity-expense-tracker.herokuapp.com/api"
+// export const BaseURL= "http://localhost:8080/api"
+// export const BaseURL= "https://techivity.herokuapp.com/api"
+export const BaseURL="https://productivity-api-server-sneygk5cqq-uc.a.run.app/api"
