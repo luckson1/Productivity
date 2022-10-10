29c2ed9a42c3e2768cdd9abf817f64a4b0a2a37b
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-10-10 23:29:39 +0300

    fix: change hooks for ts use

---
+++ client/src/redux/hooks.ts
@@ -0,0 +1,5 @@
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { AppDispatch, AppState } from "./Store";
+
+export const getState: TypedUseSelectorHook<AppState>= useSelector
+export const dispatch= ()=> useDispatch<AppDispatch>()
