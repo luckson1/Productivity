9198b5c594ca7a0499c88c734d6a4efd0c18fcf9
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-10-28 01:18:26 +0300

    fix: change server url

--- client/src/utils/BaseUrl.ts
+++ client/src/utils/BaseUrl.ts
@@ -1,3 +1,3 @@
-export const BaseURL= "http://localhost:4000/api"
+// export const BaseURL= "http://localhost:4000/api"
 // export const BaseURL= "https://techivity.herokuapp.com/api"
-// export const BaseURL="https://productivity-api-server-sneygk5cqq-uc.a.run.app/api"
+export const BaseURL="https://productivity-api-server-sneygk5cqq-uc.a.run.app/api"
